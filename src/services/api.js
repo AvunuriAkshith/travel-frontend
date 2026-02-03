c62d0c41e47b8dc799d2f072e635c279333970ae
AunuriAkshith <avunuriakshith939@gmail.com>
2026-02-03 22:46:59 +0530

    updated to frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://travel-backend.onrender.com/api"
+
+
 });
 
 export default API;
